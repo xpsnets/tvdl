12676dc1f886f41923c9deb8d0973097ba687133
xpsnets <xpsnets@outlook.com>
2021-03-20 19:33:05 +0800

    add timeout

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def get_data(tv,page):
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
             }
         )
-        u = urllib.request.urlopen(req).read()
+        u = urllib.request.urlopen(req, timeout=10).read()
         doc = json.loads(u)
         check_and_download(tv, doc)
         print(int(doc["torrents_count"]))
