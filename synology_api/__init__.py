ec8ea3d94e89b7fab314a4f44ca20ffbfcc3deb9
xpsnets <xpsnets@outlook.com>
2021-08-09 13:04:22 +0800

    add lib

---
+++ synology_api/__init__.py
@@ -0,0 +1,12 @@
+from . import \
+    audiostation,\
+    auth,\
+    base_api_core,\
+    core_active_backup,\
+    core_backup,\
+    core_certificate,\
+    core_sys_info,\
+    downloadstation,\
+    filestation,\
+    virtualization
+
---
+++ synology_api/core_backup.py
@@ -0,0 +1,47 @@
+from . import base_api_core
+
+
+class Backup(base_api_core.Core):
+    def __init__(self, ip_address, port, username, password, secure=False, cert_verify=False, dsm_version=2):
+        super(Backup, self).__init__(ip_address, port, username, password, secure, cert_verify, dsm_version)
+
+    def backup_repository_get(self):
+        api_name = 'SYNO.Backup.Repository'
+        info = self.core_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'get'}
+
+        return self.request_data(api_name, api_path, req_param)
+        
+    def backup_repository_list(self):
+        api_name = 'SYNO.Backup.Repository'
+        info = self.core_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'list'}
+
+        return self.request_data(api_name, api_path, req_param)
+        
+    def backup_task_list(self):
+        api_name = 'SYNO.Backup.Task'
+        info = self.core_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'list'}
+
+        return self.request_data(api_name, api_path, req_param)
+        
+    def backup_task_status(self):
+        api_name = 'SYNO.Backup.Task'
+        info = self.core_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'status'}
+
+        return self.request_data(api_name, api_path, req_param)
+        
+    def backup_task_get(self):
+        api_name = 'SYNO.Backup.Task'
+        info = self.core_list[api_name]
+        api_path = info['path']
+        req_param = {'version': info['maxVersion'], 'method': 'get'}
+
+        return self.request_data(api_name, api_path, req_param)
+
